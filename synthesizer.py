1faebdd7fc8e307cc501bead588cee6ec3337d52
unknown <zealan@gmail.com>
2026-04-26 17:31:09 -0500

    various tests for the writeup

--- synthesizer.py
+++ synthesizer.py
@@ -38,35 +38,34 @@ def distance(data):
 
 good_rules = [('j', 'u'), ('ʊj', 'ʊw'), ('ʊb', 'ub'), ('cu', 'cf'), ('bʊ', 'by'), ('aj', 'az'), ('pa', 'ba'), ('ʊc', 'uc'), ('j', 'ɲ'), ('ki', 'k'), ('ʊt', 'ut'), ('ji', 'ki'), ('ʊ', 'w'), ('up', 'uf'), ('ʊe', 'we'), ('jɪ', 'j'), ('de', 're'), ('jʊ', 'ju'), ('dʊ', 'du'), ('ku', 'vu'), ('cu', 'c'), ('ɪm', 'im'), ('ɪk', 'ik'), ('ʊj', 'ʊk'), ('j', ''), ('ʊd', 'ʊr'), ('ʊ', 'u'), ('do', 'zo'), ('pa', 'fa'), ('dʊ', 'dw'), ('cu', 'fu'), ('tʊ', 'tw'), ('id', 'ir'), ('po', 'ho'), ('je', 'we'), ('ki', 'i'), ('ca', 'sa'), ('ge', 'gi'), ('ge', 'ʒe'), ('ɪj', 'j'), ('kɪ', 'ki'), ('uɪ', 'ɪ'), ('ʊe', 'ue'), ('ad', 'ar'), ('do', 'ro'), ('ti', 'si'), ('ji', 'ʒi'), ('bu', 'vu'), ('ʊn', 'on'), ('j', 'i'), ('gʊ', 'gw'), ('ʊj', 'ʊu'), ('ic', 'if'), ('ed', 'er'), ('c', 'š'), ('ʊj', 'j'), ('jɪ', 'iɪ'), ('gu', 'vu'), ('c', 's'), ('ki', 'kć'), ('ap', 'ah'), ('ʊa', 'wa'), ('ɪj', 'ɪć'), ('cu', 'ci'), ('ce', 'se'), ('kʊ', 'ku'), ('ʊd', 'ud'), ('ʊc', 'ʊs'), ('cu', 'u'), ('ɪg', 'ig'), ('pa', 'ha'), ('jɪ', 'zɪ'), ('c', 'ʃ'), ('jʊ', 'oʊ'), ('ɪd', 'id'), ('ʊj', 'ʊ'), ('bʊ', 'bw'), ('ac', 'as'), ('di', 'zi'), ('ʊn', 'un'), ('ʊi', 'wi'), ('nɲ', 'ɲ'), ('jʊ', 'ɲʊ'), ('ʊg', 'ug'), ('ɪt', 't'), ('ja', 'a'), ('od', 'or'), ('da', 'ra'), ('ic', 'iš'), ('tʊ', 'tu'), ('jɪ', 'ji'), ('gɪ', 'gi'), ('nj', 'nɲ'), ('ji', 'ui'), ('ʊ', 'y'), ('ɪd', 'ɪr'), ('jo', 'zo'), ('ɪ', 'ɟ'), ('p', 'b'), ('c', 'f'), ('ia', 'ya'), ('ji', 'zi'), ('ji', 'wi'), ('ci', 'ʃi'), ('ʊj', 'ʊi'), ('ai', 'a'), ('dɪ', 'dy'), ('mʊ', 'mu'), ('ʊj', 'wj'), ('ag', 'ab'), ('p', 'h'), ('ɪj', 'ćj'), ('d', 'z'), ('uɪ', 'iɪ'), ('kʊ', 'kw'), ('gɪ', 'gɟ'), ('tu', 'ču'), ('gu', 'mu'), ('ɪb', 'ib'), ('ji', 'i'), ('jʊ', 'jo'), ('up', 'uw'), ('ʊm', 'um'), ('cʊ', 'cw'), ('d', 'r'), ('cʊ', 'sʊ'), ('ji', 'ii'), ('ɪ', 'i'), ('j', 'z'), ('ʊj', 'uj'), ('ɪt', 'it'), ('un', 'u'), ('ed', 'ez'), ('ku', 'gu'), ('ʊk', 'uk'), ('dɪ', 'di'), ('ʊe', 'ye'), ('ud', 'ur'), ('uɪ', 'u'), ('tɪ', 'ti'), ('ij', 'iz'), ('pa', 'wa'), ('jo', 'wo'), ('ku', 'pu'), ('co', 'so'), ('mʊ', 'mw'), ('ɪ', 'y'), ('ip', 'ib'), ('jo', 'o'), ('ja', 'wa'), ('oi', 'oe'), ('uɪ', 'ui'), ('dʊ', 'rʊ'), ('ki', 'ći'), ('ʊ', 'o'), ('eg', 'ig'), ('jʊ', 'zʊ'), ('ja', 'za'), ('ap', 'ab'), ('dɪ', 'rɪ'), ('ip', 'ih'), ('gi', 'zi'), ('kɪ', 'kć'), ('gʊ', 'gu'), ('ɪ', 'ć'), ('j', 'w'), ('uɪ', 'fɪ'), ('nʊ', 'nw'), ('bi', 'by'), ('ti', 'ci'), ('di', 'ri'), ('ɪj', 'ɪ'), ('mo', 'mw'), ('je', 'e'), ('oi', 'wi'), ('co', 'šo'), ('nj', 'nz'), ('bʊ', 'bu'), ('jɪ', 'ɪ')]
 
-start_time = time.perf_counter()
+print(distance(data))
 
-ATTEMPTS = 10000
-best_program = []
-best_score = 10000000
-for i in range(ATTEMPTS):
-    if i % 100 == 0:
-        print('.', end='', flush=True)
-    attempt_data = data.copy()
-    rules = good_rules.copy()
-    random.shuffle(rules)
-    program = []
-    score_to_beat = distance(data)
-    for rule in rules:
-        data_after_rule = [[entry[0], entry[1].replace(rule[0], rule[1]), entry[2], entry[3]] for entry in attempt_data]
-        score_after_rule = distance(data_after_rule)
-        if score_after_rule < score_to_beat:
-            program.append(rule)
-            attempt_data = data_after_rule
-            score_to_beat = score_after_rule
-    if score_to_beat < best_score:
-        best_program = program
-        best_score = score_to_beat
-        print(best_score)
+# ATTEMPTS = 100
+# best_program = []
+# best_score = 10000000
+# for i in range(ATTEMPTS):
+#     if i % 100 == 0:
+#         print('.', end='', flush=True)
+#     attempt_data = data.copy()
+#     rules = good_rules.copy()
+#     random.shuffle(rules)
+#     program = []
+#     score_to_beat = distance(data)
+#     for rule in rules:
+#         data_after_rule = [[entry[0], entry[1].replace(rule[0], rule[1]), entry[2], entry[3]] for entry in attempt_data]
+#         score_after_rule = distance(data_after_rule)
+#         if score_after_rule < score_to_beat:
+#             program.append(rule)
+#             attempt_data = data_after_rule
+#             score_to_beat = score_after_rule
+#     if score_to_beat < best_score:
+#         best_program = program
+#         best_score = score_to_beat
+#         print(best_score)
+#     if best_score == 0:
+#         break
 
-end_time = time.perf_counter()
-print(f"Elapsed time: {end_time - start_time:.4f} seconds")
-
-print(best_program, best_score)
-for rule in best_program:
-    data = [[entry[0], entry[1].replace(rule[0], rule[1]), entry[2], entry[3]] for entry in data]
-print(data)
+# print(best_program, best_score)
+# for rule in best_program:
+#     data = [[entry[0], entry[1].replace(rule[0], rule[1]), entry[2], entry[3]] for entry in data]
+# print(data)
